a5796dd42dc52d3774b6b634601a19b772a3c671
Vova Sitnikov <sitnikov.vladimiir@mail.ru>
2022-02-09 14:25:44 +0700

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double result;
+
+
+        Console.WriteLine("Enter first number");
+        var a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter operation");
+        var operation = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine("Enter second number");
+        var b = Convert.ToDouble(Console.ReadLine());
+
+
+
+        if (operation == '+')
+        {
+            result = a + b;
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+
+        else if (operation == '-')
+        {
+            result = a - b;
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+
+        else if (operation == '*')
+        {
+            result = a * b;
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+
+        else if (operation == '/')
+        {
+            result = a / b;
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+
+        else if (operation == '^')
+        {
+            result = Math.Pow(a, b);
+            Console.WriteLine($"{a} {operation} {b} = {result}");
+        }
+        else
+        {
+            Console.WriteLine($"Error operator");
+        }
+    }
+
+
+}
